3a4f4c94d9f654df01256d7681fdd4fb06fce6f7
Krzychuk9 <kasprowski.krzysztof@gmail.com>
2016-08-24 19:57:01 +0200

    added hello name

--- HelloWorld/src/pl/kasprowski/helloworld/HelloWorld.java
+++ HelloWorld/src/pl/kasprowski/helloworld/HelloWorld.java
@@ -5,10 +5,15 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		HelloWorld hello = new HelloWorld();
 		hello.printHelloWorld();
+		hello.printHelloName("gruby");
 	}
-	
+
 	public void printHelloWorld() {
-		System.out.println("hello world!!");		
+		System.out.println("hello world!!");
+	}
+
+	public void printHelloName(String name) {
+		System.out.println("hello " + name);
 	}
 }
 // static import
