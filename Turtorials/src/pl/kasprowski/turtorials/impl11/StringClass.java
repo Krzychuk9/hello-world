8b53e0cdfe8ff02485e499d65ad3249cccacb330
Krzychuk9 <kasprowski.krzysztof@gmail.com>
2016-09-03 12:13:56 +0200

    Added part of string class turtorial

---
+++ Turtorials/src/pl/kasprowski/turtorials/impl11/StringClass.java
@@ -0,0 +1,9 @@
+package pl.kasprowski.turtorials.impl11;
+
+public class StringClass {
+	private String helloworld = "Hello World!";
+	char [] h = { 'h', 'e', 'l', 'l', 'o' };
+	private String hello = new String(h);
+	
+	
+}
